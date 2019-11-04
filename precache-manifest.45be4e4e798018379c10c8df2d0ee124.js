ea96ade2d257218495a36dfda3ea78c81d737954
Alexandre de Marco <alexandredmarco@gmail.com>
2019-11-04 08:57:02 -0300

    Updates

---
+++ precache-manifest.45be4e4e798018379c10c8df2d0ee124.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a7a0ff03d96eb73f72e28f3d94aec9",
+    "url": "/tempspinpay/index.html"
+  },
+  {
+    "revision": "db5fad8b6f117def6633",
+    "url": "/tempspinpay/static/js/2.66eb1b80.chunk.js"
+  },
+  {
+    "revision": "374d527a47e3aef81d1d",
+    "url": "/tempspinpay/static/js/main.f1ac9193.chunk.js"
+  },
+  {
+    "revision": "cc7f30999345ecf0107d",
+    "url": "/tempspinpay/static/js/runtime-main.85d51986.js"
+  },
+  {
+    "revision": "491279e3aab3e390028f2ee3ca5da4cf",
+    "url": "/tempspinpay/static/media/COG.491279e3.gif"
+  },
+  {
+    "revision": "43cee55d73daa544dce82fb09a790540",
+    "url": "/tempspinpay/static/media/Laptop.43cee55d.webm"
+  },
+  {
+    "revision": "aafc5106cd4cbf6bdd8a08ef5a5b3eed",
+    "url": "/tempspinpay/static/media/Mobile.aafc5106.webm"
+  },
+  {
+    "revision": "6b392da5cc613a8989c02abdcdf74a7d",
+    "url": "/tempspinpay/static/media/comp.6b392da5.png"
+  },
+  {
+    "revision": "5117a5cd475b5e0483cdd2bf171f110e",
+    "url": "/tempspinpay/static/media/dev-img.5117a5cd.png"
+  },
+  {
+    "revision": "91a23fbc2d72a1cb0188943a5f406bd9",
+    "url": "/tempspinpay/static/media/mobile-menu-icon.91a23fbc.svg"
+  },
+  {
+    "revision": "3720b5c3b9221491857e2054699094d7",
+    "url": "/tempspinpay/static/media/mobile_video.3720b5c3.mp4"
+  },
+  {
+    "revision": "9cc7a323209bd73262f0f623b7f88f1d",
+    "url": "/tempspinpay/static/media/phone.9cc7a323.png"
+  },
+  {
+    "revision": "0305f092d6de4866735d294b72127fba",
+    "url": "/tempspinpay/static/media/web_video.0305f092.mp4"
+  }
+]);
